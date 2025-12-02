0784403fe925b22bd118ce393223d60185376706
osetkursat <osetkursat@gmail.com>
2025-12-02 22:53:13 +0300

    Refactor server to use environment variable for PORT

--- server.js
+++ server.js
@@ -420,6 +420,9 @@ app.post("/admin/update-user", isAdmin, (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Sulama Asistanı Çalışıyor → http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Sulama Asistanı Çalışıyor → http://localhost:${PORT}`);
 });
+
